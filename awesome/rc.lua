4992655e84dff846f45c69e8df72e6ba6c8c337d
mirak51 <mirak51@laposte.net>
2020-10-20 06:35:49 +0200

    ok

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -177,7 +177,9 @@ awful.screen.connect_for_each_screen(function(s)
     set_wallpaper(s)
 
     -- Each screen has its own tag table.
-    awful.tag({ "1", "2", "3", "4", "5", "6", "7", "8", "9" }, s, awful.layout.layouts[1])
+    --awful.tag({ "1", "2", "3", "4", "5", "6", "7", "8", "9" }, s, awful.layout.layouts[1])
+
+    awful.tag({ "1", "2", "3", "4" }, s, awful.layout.layouts[1])
 
     -- Create a promptbox for each screen
     s.mypromptbox = awful.widget.prompt()
@@ -644,7 +646,7 @@ awful.spawn.with_shell("/usr/bin/lxpolkit")
 awful.spawn.with_shell("numlockx on")
 
  -- awful.spawn.with_shell("volumeicon")
---awful.spawn.with_shell("nm-applet")
+awful.spawn.with_shell("nm-applet")
 
 --awful.spawn.with_shell("expressvpn connect fr")
 
